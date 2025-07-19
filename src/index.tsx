082df19b8f10aad9729f3271454c360fe7904e56
chen3283891376 <3283891376@qq.com>
2025-07-19 18:16:43 +0800

    refactor: 重构index.tsx以优化图片展示

--- src/index.tsx
+++ src/index.tsx
@@ -9,6 +9,7 @@ const App = () => {
         let ignore = false;
         const func = async () => {
             const response = await fetch('https://api.github.com/repos/InverseSkyTeam/ins-hub/contents/images');
+            // const response = await fetch('https://api.github.com/repos/ClassIsland/ClassIsland-hub/contents/images');
             const data = await response.json();
             if (!ignore) setImgs(data);
         };
@@ -24,10 +25,30 @@ const App = () => {
             <Typography variant="h3" component="div">
                 INS Hub
             </Typography>
-            <div style={{ display: 'grid', gridTemplateColumns: 'repeat(4, 1fr)', gap: '16px' }}>
+            <div
+                style={{
+                    columnCount: 4,
+                    columnGap: '16px',
+                }}
+            >
                 {imgs.map((img: any, index) => (
-                    <Card key={index}>
-                        <CardMedia image={img.download_url.replace("https://raw.githubusercontent.com/ClassIsland/classisland-hub/main/images/", "./images/")} />
+                    <Card
+                        key={index}
+                        style={{
+                            display: 'inline-block',
+                            width: '100%',
+                            marginBottom: '16px',
+                            textAlign: 'center',
+                            breakInside: 'avoid',
+                            padding: '16px',
+                        }}
+                    >
+                        <CardMedia
+                            component="img"
+                            alt={img.name}
+                            loading='lazy'
+                            src={img.download_url.replace("https://raw.githubusercontent.com/InverseSkyTeam/ins-hub/main/images/", "./images/")}
+                        />
                         <CardContent>
                             <Typography variant="h5" component="div">
                                 {img.name}
