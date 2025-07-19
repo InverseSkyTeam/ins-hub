879fd398acebe6acdecd2669adeb25a8ec56ac8f
lrs2187 <liurongshuo2022@outlook.com>
2025-07-19 17:44:37 +0800

    refactor: 优化项目结构

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,6 @@
-import * as React from'react';
+import * as React from 'react';
 import { createRoot } from 'react-dom/client';
-import { Card,Container,Typography,CardMedia,CardContent } from '@mui/material';
-import { v4 as generateUUID } from 'uuid';
+import { Card, Container, Typography, CardMedia, CardContent } from '@mui/material';
 import 'tailwindcss/index.css';
 
 const App = () => {
@@ -9,23 +8,25 @@ const App = () => {
     React.useEffect(() => {
         let ignore = false;
         const func = async () => {
-            const response = await fetch('https://api.github.com/repos/ClassIsland/classisland-hub/contents/images');
+            const response = await fetch('https://api.github.com/repos/InverseSkyTeam/ins-hub/contents/images');
             const data = await response.json();
             if (!ignore) setImgs(data);
         };
+        
         if (!ignore) func().then(() => null);
         return () => {
             ignore = true;
         };
     }, []);
+
     return (
         <Container>
             <Typography variant="h3" component="div">
                 INS Hub
             </Typography>
-            <div style={{ display: 'grid', gridTemplateColumns: "repeat(4, 1fr)", gap: '16px' }}>
-                {imgs.map((img: any) => (
-                    <Card key={generateUUID()}>
+            <div style={{ display: 'grid', gridTemplateColumns: 'repeat(4, 1fr)', gap: '16px' }}>
+                {imgs.map((img: any, index) => (
+                    <Card key={index}>
                         <CardMedia image={img.download_url} />
                         <CardContent>
                             <Typography variant="h5" component="div">
@@ -36,8 +37,11 @@ const App = () => {
                 ))}
             </div>
         </Container>
-    )
+    );
 };
 
-const root = createRoot(document.getElementById('app') as HTMLElement);
-root.render(<App />);
+const dom = document.getElementById('app');
+if (dom) {
+    const root = createRoot(dom);
+    root.render(<App />);
+}
