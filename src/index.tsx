c41af5c501b8d465bd450338aefb06b53a81a0d0
有思想的不定积分 <2947799742@qq.com>
2025-07-20 17:27:51 +0800

    feat: 添加 标题栏 背景色

--- src/index.tsx
+++ src/index.tsx
@@ -28,7 +28,7 @@ const App = () => {
 
     return (
         <Container>
-            <div style={{ display: 'flex', marginBottom: '16px' }}>
+            <div style={{ display: 'flex', marginBottom: '16px', backgroundcolor: 'black' }}>
                 <img src="./images/output.webp" loading='lazy' alt="logo" style={{ height: '50px' }} />
                 <Typography variant="h3" component="div">
                     INS Hub
