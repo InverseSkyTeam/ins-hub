e832944998662ec539d7148fdc7bc97f49d8b008
BlocksFunction <3398817447@qq.com>
2025-07-20 22:00:20 +0800

    加上了切换主题动画

--- src/components/ThemeModeButton.tsx
+++ src/components/ThemeModeButton.tsx
@@ -23,8 +23,48 @@ export default function ThemeModeButton() {
     const Icon = isDark ? SunIcon : MoonStar;
     const iconColor = isDark ? 'text-yellow-500' : 'text-blue-500';
 
+    const enableTransitions = () =>
+        "startViewTransition" in document &&
+        window.matchMedia("(prefers-reduced-motion: no-preference)").matches;
+
+    async function toggleDark({ clientX: x, clientY: y }: MouseEvent) {
+        const isDark = document.documentElement.classList.contains('dark');
+
+        if (!enableTransitions()) {
+            setIsDark(!isDark)
+            return;
+        }
+
+        const endRadius = Math.hypot(
+            Math.max(x, window.innerWidth - x),
+            Math.max(y, window.innerHeight - y),
+        );
+
+        const clipPath = [
+            `circle(0px at ${x}px ${y}px)`,
+            `circle(${endRadius}px at ${x}px ${y}px)`,
+        ];
+
+        await document.startViewTransition(() => {
+            setIsDark(!isDark)
+        }).ready;
+
+        document.documentElement.animate(
+            {
+                clipPath: isDark ? clipPath : [...clipPath].reverse(),
+            },
+            {
+                duration: 700,
+                easing: "ease-in",
+                pseudoElement: isDark
+                    ? "::view-transition-new(root)"
+                    : "::view-transition-old(root)",
+            },
+        );
+    }
+
     return (
-        <button onClick={() => setIsDark(!isDark)} aria-label={isDark ? '切换到亮色模式' : '切换到暗色模式'}>
+        <button onClick={toggleDark} aria-label={isDark ? '切换到亮色模式' : '切换到暗色模式'}>
             <Icon className={iconColor} size={35} />
         </button>
     );
