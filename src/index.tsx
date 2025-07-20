d35be29dbe5af0e83953024950dd1ce1ec7af3a6
有思想的不定积分 <2947799742@qq.com>
2025-07-20 17:22:58 +0800

    Update index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -50,7 +50,7 @@ const App = () => {
                         style={{
                             textAlign: 'center',
                             padding: '16px',
-                            cursor: pointer,
+                            cursor: 'pointer',
                         }}
                         onClick={() => {
                             window.open(`./images/${img.name}`);
