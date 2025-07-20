e7525bd089239c2fc3009c003c66de47019db43c
chen3283891376 <3283891376@qq.com>
2025-07-20 18:52:39 +0800

    feat(ui): 更新背景样式和滚动条样式

--- src/index.tsx
+++ src/index.tsx
@@ -7,7 +7,6 @@ import "./styles/base.css";
 const App = () => {
     const [imgs, setImgs] = React.useState<any[]>([]);
     React.useEffect(() => {
-        document.body.style.background = 'linear-gradient(to right bottom,rgba(111,255,255,0.2),rgba(111,255,189,0.3),rgba(255,233,111,0.4))';
         let ignore = false;
         const func = async () => {
             const response = await fetch('https://api.github.com/repos/InverseSkyTeam/ins-hub/contents/images');
@@ -29,26 +28,27 @@ const App = () => {
     }, []);
 
     return (
+        <>
+        <div style={{ display: 'flex', marginBottom: '16px', backgroundColor: '#141428' }}>
+            <img src="./images/output.webp" loading='lazy' alt="logo" style={{ height: '50px' }} />
+            <Typography variant="h3" component="div" color="#66faca">
+                INS Hub
+            </Typography>
+            <div style={{ flexGrow: 1 }} />
+            <Button
+                variant="outlined"
+                onClick={() => {
+                    window.open('https://github.com/InverseSkyTeam/ins-hub/upload/master/images');
+                }}
+                style={{
+                    margin: 4,
+                    backgroundColor: '#efefef'
+                }}
+            >
+                前往 GitHub 上传图片
+            </Button>
+        </div>
         <Container>
-            <div style={{ display: 'flex', marginBottom: '16px', backgroundColor: '#141428' }}>
-                <img src="./images/output.webp" loading='lazy' alt="logo" style={{ height: '50px' }} />
-                <Typography variant="h3" component="div" color="#66faca">
-                    INS Hub
-                </Typography>
-                <div style={{ flexGrow: 1 }} />
-                <Button
-                    variant="outlined"
-                    onClick={() => {
-                        window.open('https://github.com/InverseSkyTeam/ins-hub/upload/master/images');
-                    }}
-                    style={{
-                        margin: 4,
-                        backgroundColor: '#efefef'
-                    }}
-                >
-                    前往 GitHub 上传图片
-                </Button>
-            </div>
             <Masonry columns={4} spacing={2}>
                 {imgs.map((img: any, index) => (
                     <Card
@@ -56,13 +56,18 @@ const App = () => {
                         style={{
                             textAlign: 'center',
                             padding: '16px',
-                            cursor: 'pointer',
-                        }}
-                        onClick={() => {
-                            window.open(`./images/${img.name}`);
                         }}
                     >
-                        <CardMedia component="img" alt={img.name} loading="lazy" src={`./images/${img.name}`} />
+                        <CardMedia 
+                            component="img" 
+                            alt={img.name} 
+                            loading="lazy" 
+                            src={`./images/${img.name}`} 
+                            style={{ cursor: 'pointer' }}
+                            onClick={() => {
+                                window.open(`./images/${img.name}`);
+                            }}
+                        />
                         <CardContent>
                             <Typography variant="h5" component="div">
                                 {img.name}
@@ -72,6 +77,7 @@ const App = () => {
                 ))}
             </Masonry>
         </Container>
+        </>
     );
 };
 
